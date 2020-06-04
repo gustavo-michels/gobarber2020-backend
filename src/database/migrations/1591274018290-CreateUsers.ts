48676f10340c88cbaa3e59581ee0d497ecfe87ac
Gustavo Henrique Michels <ghmichels@gmail.com>
2020-06-04 09:57:39 -0300

    add users migration

--- src/database/migrations/1591274018290-CreateUsers.ts
+++ src/database/migrations/1591274018290-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1591234360322
-  implements MigrationInterface {
+export default class CreateUsers1591274018290 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,12 +14,18 @@ export default class CreateAppointments1591234360322
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            isUnique: true,
           },
           {
             name: 'created_at',
@@ -38,6 +43,6 @@ export default class CreateAppointments1591234360322
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
