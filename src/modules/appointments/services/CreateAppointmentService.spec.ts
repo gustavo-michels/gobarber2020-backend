3c0bdb491adc0fe31864618d605b0b2fb5a45943
Gustavo H. Michels <ghmichels@gmail.com>
2020-06-30 09:55:26 -0300

    fix jest config

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,23 @@
+import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointmentService.execute({
+      date: new Date(),
+      provider_id: 'feda49f1-413b-4a40-b5b8-ae9ba0247128',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe(
+      'feda49f1-413b-4a40-b5b8-ae9ba0247128',
+    );
+  });
+
+  // it('should not be able to create two appointments on the same time', () => {});
+});
