40dc0cbdc7c2037721a8dcc428a31adc79ff3007
Gustavo Henrique Michels <ghmichels@gmail.com>
2020-08-12 20:06:55 -0300

    filter available appointments

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -25,7 +25,47 @@ describe('ListProviderMonthAvailability', () => {
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user-id-1',
+      date: new Date(2020, 4, 20, 9, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user-id-1',
       date: new Date(2020, 4, 20, 10, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user-id-1',
+      date: new Date(2020, 4, 20, 11, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user-id-1',
+      date: new Date(2020, 4, 20, 12, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user-id-1',
+      date: new Date(2020, 4, 20, 13, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user-id-1',
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user-id-1',
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user-id-1',
+      date: new Date(2020, 4, 20, 16, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user-id-1',
+      date: new Date(2020, 4, 20, 17, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
@@ -41,10 +81,10 @@ describe('ListProviderMonthAvailability', () => {
 
     expect(availability).toEqual(
       expect.arrayContaining([
-        { day: 19, availability: true },
-        { day: 20, availability: false },
-        { day: 21, availability: false },
-        { day: 22, availability: true },
+        { day: 19, available: true },
+        { day: 20, available: false },
+        { day: 21, available: true },
+        { day: 22, available: true },
       ]),
     );
   });
