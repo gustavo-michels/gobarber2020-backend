fe733452640d77bbbf82d05633d65544291005fd
Gustavo H. Michels <ghmichels@gmail.com>
2020-08-13 11:29:24 -0300

    att

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -15,8 +15,12 @@ describe('CreateAppointment', () => {
   });
 
   it('should be able to create a new appointment', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
     const appointment = await createAppointmentService.execute({
-      date: new Date(),
+      date: new Date(2020, 4, 10, 13),
       provider_id: 'feda49f1-413b-4a40-b5b8-ae9ba0247128',
       user_id: '9af66648-cff7-48fb-a84a-a41c9a4b7d09',
     });
@@ -28,6 +32,10 @@ describe('CreateAppointment', () => {
   });
 
   it('should not be able to create two appointments on the same time', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 8).getTime();
+    });
+
     const appointmentDate = new Date(2020, 5, 10, 11); // 10/06/2020 11:00:00
 
     await createAppointmentService.execute({
@@ -41,6 +49,56 @@ describe('CreateAppointment', () => {
         date: appointmentDate,
         provider_id: '05024d2f-5d17-4e0c-8c51-c9755eddfe28',
         user_id: '9af66648-cff7-48fb-a84a-a41c9a4b7d09',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create an appointment on a past date', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointmentService.execute({
+        date: new Date(2020, 4, 10, 10),
+        provider_id: '05024d2f-5d17-4e0c-8c51-c9755eddfe28',
+        user_id: '9af66648-cff7-48fb-a84a-a41c9a4b7d09',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create an appointment with same user as provider', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointmentService.execute({
+        date: new Date(2020, 4, 10, 13),
+        provider_id: '05024d2f-5d17-4e0c-8c51-c9755eddfe28',
+        user_id: '05024d2f-5d17-4e0c-8c51-c9755eddfe28',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create an appointment before 8am and after 5pm', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointmentService.execute({
+        date: new Date(2020, 4, 11, 7),
+        provider_id: 'feda49f1-413b-4a40-b5b8-ae9ba0247128',
+        user_id: '05024d2f-5d17-4e0c-8c51-c9755eddfe28',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+
+    await expect(
+      createAppointmentService.execute({
+        date: new Date(2020, 4, 11, 18),
+        provider_id: 'feda49f1-413b-4a40-b5b8-ae9ba0247128',
+        user_id: '05024d2f-5d17-4e0c-8c51-c9755eddfe28',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
